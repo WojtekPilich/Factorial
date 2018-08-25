c631859f262580dcc8afbefbfec9e07c5671db4e
Wojtek Pilich <chilip@tlen.pl>
2018-08-25 20:59:11 +0200

    test corrections

--- tests/unit/FactorialTest.php
+++ tests/unit/FactorialTest.php
@@ -5,14 +5,14 @@ class FactorialTest extends PHPUnit\Framework\TestCase
     public function testResultOfFactorialRecursiveFunction()
     {
         $testFactorial1 = new \App\Factorial();
-        $this->assertEquals($testFactorial1->recursive(3), 6); // passed
-        $this->assertEquals($testFactorial1->recursive(9), 3628800); // failed, result is for number 10
+        $this->assertEquals($testFactorial1->recursive(3), 6);
+        $this->assertNotEquals($testFactorial1->recursive(9), 3628800);
     }
 
     public function testResultOfFactorialNonRecursiveFunction()
     {
         $testFactorial2 = new \App\Factorial();
-        $this->assertEquals($testFactorial2->non_recursive(5), 120); // passed
-        $this->assertEquals($testFactorial2->non_recursive(4), 20); // failed, result should be 24
+        $this->assertEquals($testFactorial2->non_recursive(5), 120);
+        $this->assertNotEquals($testFactorial2->non_recursive(4), 20);
     }
 }
